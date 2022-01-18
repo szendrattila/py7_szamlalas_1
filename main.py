9138c8a696457f75a200756091540d792b6cffaa
szendrattila <71691690+szendrattila@users.noreply.github.com>
2022-01-18 08:28:55 +0000

    értelmező lista

--- main.py
+++ main.py
@@ -3,15 +3,12 @@
 Készíts egy programot, amely [1;10] intervallumon generál 5 darab véletlen egész számot és ezeket tárolja el egy listában! A program számolja össze, és képernyőre írja ki a listában tárolt páros számok számát, valamint a lista elemeit!
 '''
 import random
-lista = []
-paroslista = []
-for i in range(5):
-    veletlen = random.randint(1, 10)
-    lista.append(veletlen)
-    print(veletlen)
-for i in lista:
-    if i % 2 == 0:
-        paroslista.append(i)
+
+lista = [random.randint(1, 10) for i in range(5) ]
+
+paroslista = [i for i in lista if i % 2 == 0]
+
+
 print(f"A mennyi szám van a listában:{len(paroslista)} és a páros számok: {paroslista}")
 
 
